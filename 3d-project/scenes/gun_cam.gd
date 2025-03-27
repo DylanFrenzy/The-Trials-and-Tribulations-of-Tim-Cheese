d24cbd06f2939d2256236381c877783bc51bf4cd
dylan <C24486954@myTUDublin.ie>
2025-03-27 18:03:34 +0000

    animation work continued

--- 3d-project/scenes/gun_cam.gd
+++ 3d-project/scenes/gun_cam.gd
@@ -6,10 +6,11 @@ func _ready():
     pass
 
 func _process(delta):
-    Node_3D.position.x = lerp(Node_3D.position.x, 0.0, delta * 5)
-    Node_3D.position.y = lerp(Node_3D.position.y, 0.0, delta * 5)
+    Node_3D.position.x = lerp(Node_3D.position.x, 0.204, delta * 5)
+    Node_3D.position.y = lerp(Node_3D.position.y, -0.17, delta * 5)
     pass
 
 func sway(sway_amount):
-    Node_3D.position.x += sway_amount.x * 0.005
-    Node_3D.position.y += sway_amount.y * 0.005
+    Node_3D.position.x -= sway_amount.x * 0.0002
+    Node_3D.position.y += sway_amount.y * 0.0002
+
