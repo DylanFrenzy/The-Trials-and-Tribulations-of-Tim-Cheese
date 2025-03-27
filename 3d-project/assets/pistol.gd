7ebde3ea9048dd8ce483f0628a3c8809ebcfd6fd
dylan <C24486954@myTUDublin.ie>
2025-03-27 21:54:03 +0000

    stuff

---
+++ 3d-project/assets/pistol.gd
@@ -0,0 +1,32 @@
+extends Node3D
+
+@export var damage := 10
+@export var max_ammo := 10
+@export var current_ammo := 10
+@export var reload_time := 1.0
+
+var is_reloading := false
+
+func _ready():
+    $AnimationPlayer.play("idle")
+
+func shoot():
+    if is_reloading or current_ammo <= 0:
+        return
+    
+    current_ammo -= 1
+    $AnimationPlayer.stop()
+    $AnimationPlayer.play("shoot")
+    # Play sound, muzzle flash, etc.
+    
+    # Raycast shooting would be handled by the player script
+
+func reload():
+    if is_reloading or current_ammo == max_ammo:
+        return
+    
+    is_reloading = true
+    $AnimationPlayer.play("reload")
+    await $AnimationPlayer.animation_finished
+    current_ammo = max_ammo
+    is_reloading = false
--- 3d-project/scripts/gun_cam.gd
+++ 3d-project/scripts/gun_cam.gd
@@ -2,19 +2,22 @@ extends Camera3D
 
 @onready var Node_3D = $Node3D
 @onready var animation_player = $Node3D/Pistol/AnimationPlayer2
+@onready var Muzzle_Flash = $muzzle_flash
 
 func _ready():
-    pass
+	pass
 
 func _process(delta):
-    Node_3D.position.x = lerp(Node_3D.position.x, 0.204, delta * 5)
-    Node_3D.position.y = lerp(Node_3D.position.y, -0.17, delta * 5)
-    pass
+	Node_3D.position.x = lerp(Node_3D.position.x, 0.204, delta * 5)
+	Node_3D.position.y = lerp(Node_3D.position.y, -0.17, delta * 5)
+	pass
 
 func sway(sway_amount):
-    Node_3D.position.x -= sway_amount.x * 0.0002
-    Node_3D.position.y += sway_amount.y * 0.0002
+	Node_3D.position.x -= sway_amount.x * 0.0002
+	Node_3D.position.y += sway_amount.y * 0.0002
 
 func _input(event: InputEvent) -> void:
-    if (event.is_action_pressed("shoot")):
-        animation_player.play("Fire")
+	if (event.is_action_pressed("shoot")):
+		if animation_player.is_playing(): return
+		animation_player.play("Fire")
+		Muzzle_Flash.emitting = true
