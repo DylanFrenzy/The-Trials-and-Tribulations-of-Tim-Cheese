8111b2484182af1edcb36f7713add624ad1f8c16
dylan <C24486954@myTUDublin.ie>
2025-04-07 14:14:51 +0100

    Tried doing loadout, near done I think but not working

--- 3d-project/scripts/guns/Gun.gd
+++ 3d-project/scripts/guns/Gun.gd
@@ -10,7 +10,12 @@ class_name Gun
 @onready var ray_caster = get_parent().get_parent().get_node("RayCast3D");
 @onready var ammo_display = get_tree().root.get_node("Node3D/hud/Ammo")
 @onready var click_sound = get_parent().get_parent().get_node("AudioStreamPlayer3D5");
+@onready var pistol = $Pistol;
+@onready var sniper = get_parent().get_node("Sniper Rifle");
 var is_reloading := false
+var weapon_List = [0, 1];
+#var current_Weapon = weapon_List[0];
+var current_Weapon = 0
 
 func _ready():
 	update_ammo_display(current_ammo)
@@ -38,6 +43,37 @@ func _input(event):
 		current_ammo = max_ammo
 		update_ammo_display(max_ammo)
 		is_reloading = false
+
+	if event.is_action_pressed("swap_weapon"):
+		swap_weapon()
+		#if ani_player.is_playing(): 
+		#	return
+		#weapon_List.erase(0)
+		#if weapon_List.has (1):
+		#	weapon_List.append(0)
+		#if weapon_List.has (0):
+		#	weapon_List.append(1)
+		#$pistol.visible = !$pistol.visible
+		#$sniper.visible = !$sniper.visible
+
+func swap_weapon():
+	if ani_player.is_playing():
+		return
+	current_Weapon = (current_Weapon + 1) % weapon_List.size()
+	update_weapon_visibility()
+
+func update_weapon_visibility():
+	match current_Weapon:
+		0:  # Pistol
+			pistol.visible = true
+			sniper.visible = false
+		1:  # Sniper
+			pistol.visible = false
+			sniper.visible = true
+
+			
+		
+		pass
 		
 func update_ammo_display(ammo):
 	ammo_display.text = str(ammo) + "/" + str(max_ammo)
---
+++ 3d-project/scripts/guns/Loadout.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var weapon_List = [0, 1];
+var current_Weapon = Array[1];
+
+func _input(event):
+   if (event.is_action_pressed("swap_weapon")):
+    
+   
+   
+    pass
+
+
+
