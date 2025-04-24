e3570b2ecb0b9e38b9dcb994ce2c8da5d3011f0c
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-04-24 19:50:06 +0100

    added head bob + sprint

--- 3d-project/scripts/character_body_3d.gd
+++ 3d-project/scripts/character_body_3d.gd
@@ -3,7 +3,8 @@ extends CharacterBody3D
 signal player_death
 
 @export var mouse_sensitivity := 0.1
-@export var move_speed := 5.0
+@export var walk_speed := 5.0
+@export var sprint_speed := 8.0
 @export var jump_velocity := 4.5
 @export var health = 200
 
@@ -11,6 +12,10 @@ signal player_death
 @onready var GunCam = $Camera3D/SubViewportContainer/SubViewport/GunCam
 @onready var AnimationPlayer2 = GunCam.get_node("Pistol2/AnimationPlayer2")
 @onready var health_bar = get_parent().get_node("hud/HealthBar")
+
+const  HEADBOB_MOVE_AMOUT = 0.06;
+const  HEADBOB_FREQUENCY = 2.4;
+var headbobTime = 0.0;
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var zoomed = false
@@ -52,13 +57,23 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
-		velocity.x = direction.x * move_speed
-		velocity.z = direction.z * move_speed
+		velocity.x = direction.x * get_movement_speed()
+		velocity.z = direction.z * get_movement_speed()
 	else:
-		velocity.x = move_toward(velocity.x, 0, move_speed)
-		velocity.z = move_toward(velocity.z, 0, move_speed)
+		velocity.x = move_toward(velocity.x, 0, walk_speed)
+		velocity.z = move_toward(velocity.z, 0, walk_speed)
+	headbob_effect(delta)
 	
 	move_and_slide()
+	
+func get_movement_speed():
+	return sprint_speed if Input.is_action_pressed("sprint") else walk_speed
+
+func headbob_effect(delta):
+	headbobTime += delta * velocity.length()
+	var bob_x = cos(headbobTime * HEADBOB_FREQUENCY * 0.5) * HEADBOB_MOVE_AMOUT
+	var bob_y = sin(headbobTime * HEADBOB_FREQUENCY) * HEADBOB_MOVE_AMOUT
+	camera.position = Vector3(bob_x, bob_y, 0)
 	
 func take_damage(amount):
 	health = max(health - amount, 0)
