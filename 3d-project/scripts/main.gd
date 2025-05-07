f2523f56daf94ff2bcee14d0cd5a25d6c7523e33
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-05-07 16:41:27 +0100

    enemy counter

--- 3d-project/scripts/main.gd
+++ 3d-project/scripts/main.gd
@@ -3,12 +3,15 @@ extends Node3D
 @onready var johnnie : PackedScene = preload("res://assets/John_Pork.tscn")
 @onready var enemy_spawn_path = $EnemySpawnPath/PathFollow3D
 @onready var wave_display = $hud/wave_display
+@onready var enemy_counter = $hud/EnemyCounter
 var wave = 1
 var enemies_spawned = []
 
 func _ready():
 	$Player.player_death.connect(_on_player_death)
 	spawn_enemy_wave(wave);
+	set_enemy_counter()
+	
 
 func spawn_enemy_wave(wave_count):
 	wave_display.text = "Wave " + str(wave_count)
@@ -20,11 +23,15 @@ func spawn_enemy_wave(wave_count):
 		enemy_instance.position = enemy_spawn_path.position;
 		add_child(enemy_instance)
 		
+func set_enemy_counter():
+	enemy_counter.text = "Enemies remaining this wave: " + str(enemies_spawned.size())
+		
 func _on_enemy_death():
 	enemies_spawned.pop_back()
 	if enemies_spawned.is_empty():
 		wave += 1
 		spawn_enemy_wave(wave)
+	set_enemy_counter()
 		
 func _on_player_death():
 	var death_screen = preload("res://scenes/hud/death_screen.tscn").instantiate()
