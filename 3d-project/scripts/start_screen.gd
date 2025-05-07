c928c855a29cf3f7598fe451ebbfd6252018269e
dylan <C24486954@myTUDublin.ie>
2025-05-07 13:48:41 +0100

    menu music stuff

--- 3d-project/scripts/start_screen.gd
+++ 3d-project/scripts/start_screen.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var load_bar = $ProgressBar;
+@onready var menumusic = $menumusic;
 
 var main_game_scene_path;
 var progress_value = 0.00;
@@ -9,11 +10,14 @@ var is_loading = false
 func load_bar_start():
 	main_game_scene_path = "res://scenes/main.tscn"
 	ResourceLoader.load_threaded_request(main_game_scene_path)
+
+func _ready() -> void:
+	menumusic.play()
 	
 func _process(delta: float) -> void:
 	if not main_game_scene_path:
+		menumusic.stop()
 		return;
-		
 	var progress = []
 	var status = ResourceLoader.load_threaded_get_status(main_game_scene_path, progress)
 	
@@ -26,7 +30,9 @@ func _process(delta: float) -> void:
 		
 	if load_bar.value >= 99:
 		var loaded_scene = ResourceLoader.load_threaded_get(main_game_scene_path)
-		if loaded_scene: get_tree().change_scene_to_packed(loaded_scene);
+		if loaded_scene:
+			menumusic.stop()
+			get_tree().change_scene_to_packed(loaded_scene);
 			
 func _on_button_pressed() -> void:
 	$Button.disabled = true
